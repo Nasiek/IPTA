95f5bb29543714b7ec03c1d2f793d9eee0be1284
k <keisangittens@gmail.com>
2018-04-24 12:59:00 -0400

    updated Users schema with first and last names

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_24_024709) do
+ActiveRecord::Schema.define(version: 2018_04_24_165418) do
 
   create_table "ship_trip_joins", force: :cascade do |t|
     t.integer "ship_id"
@@ -55,6 +55,8 @@ ActiveRecord::Schema.define(version: 2018_04_24_024709) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "fname"
+    t.string "lname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
