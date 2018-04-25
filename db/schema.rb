892e7fe3f2149e1909528bc912c7d24117e5ca27
k <keisangittens@gmail.com>
2018-04-25 15:31:30 -0400

    removed avatar column from users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_24_171411) do
+ActiveRecord::Schema.define(version: 2018_04_25_192817) do
 
   create_table "ship_trip_joins", force: :cascade do |t|
     t.integer "ship_id"
@@ -57,7 +57,6 @@ ActiveRecord::Schema.define(version: 2018_04_24_171411) do
     t.datetime "updated_at", null: false
     t.string "fname"
     t.string "lname"
-    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
