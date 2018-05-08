eb2c0761af20c70f4f0893467fb1d54ebfea8513
Reef Daddy <campbellsharif@gmail.com>
2018-05-08 16:57:34 -0400

    yess

--- app/helpers/trips_helper.rb
+++ app/helpers/trips_helper.rb
@@ -1,12 +1,14 @@
 module TripsHelper
 
-  planet_distances = { "Venus" => 0,
+  def planet_distances
+                    { "Venus" => 0,
                       "Earth" => 25,
                       "Moon" => 25.25,
                       "Mars" => 75.25,
                       "Ceres" => 190.25,
                       "Europa" => 415.25
                       }
+    end
 
   def calculate_distance(t)
     planet_distances[t.destination] - planet_distances[t.origin]
